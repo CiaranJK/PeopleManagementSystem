0fa6bc23c875d84c6539d6a692c11b6b140cbb61
Ciaran Kieran <ciaranjohnkieran@gmail.com>
2022-09-02 12:19:20 +0100

    Delete main directory

--- main/kotlin/com/example/people_managment_system/repositories/EmployeeRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.people_managment_system.repositories
-
-import com.example.people_managment_system.model.Employee
-import org.springframework.data.repository.CrudRepository
-
-interface EmployeeRepository : CrudRepository<Employee, Int>
